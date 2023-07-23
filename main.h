4c8de68313135fed32ec6d2413f4dd3a23b6bd2d
Youness91-tech <bayttarbawi.com@gmail.com>
2023-07-23 07:03:48 -0700

    main.h updated 3

--- main.h
+++ main.h
@@ -25,6 +25,20 @@ int print_str(va_list list);
 int print_prct(va_list list);
 int print_nan(char ch1, char ch2);
 int print_int(va_list list);
+int print_bin(va_list list);
+int print_oct(va_list list);
+int print_hex(va_list list);
+int print_Hex(va_list list);
+int print_SS(va_list list);
+int print_pp(va_list list);
+int print_unsigned(va_list list);
+unsigned int base_len(unsigned int num, int base);
+int *rev_str(char *s);
+char *hex_conv(int num);
+void _write(buffer *b_r, char c);
+void _write_str(buffer *b_r, char *s);
+void _write_tmpbuf(buffer *b_r);
+int print_rr(va_list list);
 
 #endif
 
