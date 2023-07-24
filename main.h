25e435e488064de2a77e766d86c43169d3f6be90
Youness91-tech <bayttarbawi.com@gmail.com>
2023-07-24 05:46:46 -0700

    main.h created

--- main.h
+++ main.h
@@ -1,28 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
 #include <unistd.h>
 #include <limits.h>
-#include <stdlib.h>
 
-#define OUTPUT_BUFF_SIZE 1024
-#define BUFF_FLUSH -1
-
-#define NUL_STRING "(null)"
-
-#define PARAMS_INIT (0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-
-#define CONVERT_LOWERCASE 1
-#define CONVERT_UNSIGNED 2
+#define BUFFER_SIZE 1024
 
 int _printf(const char *format, ...);
+int print_char(va_list list);
+int print_str(va_list list);
+int print_num(va_list list);
+int print_bin(va_list list);
+int print_oct(va_list list);
+int print_hex(va_list list);
+int print_HEX(va_list list);
+int print_un(va_list list);
+int print_reverse(va_list list);
+int print_rot13(va_list list);
+int print_S(va_list list);
+int print_p(va_list list);
+int get_funct(va_list list, char k, char c);
 
-/****************** FUNCTIONS ******************/
-int print_ch(va_list character);
-int print_str(va_list string);
-int print_prct(va_list percent);
-int print_int(va_list integer);
-
-#endif
-
+#endif /* MAIN_H */
